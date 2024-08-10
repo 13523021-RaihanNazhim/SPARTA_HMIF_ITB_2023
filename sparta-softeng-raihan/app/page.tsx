93908fa2e5d8fee9e44fd264cb306c592890ad0b
RNXFreeze <m.raihannazhimoktana@gmail.com>
2024-08-10 21:36:53 +0700

    feat : add final version

--- sparta-softeng-raihan/app/page.tsx
+++ sparta-softeng-raihan/app/page.tsx
@@ -1,113 +1,60 @@
 import Image from "next/image";
 
-export default function Home() {
+const Home =()=> {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  <main className="flex flex-col items-center bg-stone-50 min-h-screen">
+    <div className="mt-10 mb-5">    
+      <Image
+        src="/fotopp.jpg"
+        alt="Profile Picture"
+        width="150"
+        height="225"
+        className="rounded-lg border-4 border-gray-500 shadow-lg"
+      />
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <p className="text-black text-3xl" style={{ fontFamily: 'Garamond, serif' }}> 
+      Muhammad Raihan Nazhim Oktana </p>
+    <p className="text-black text-3xl" style={{ fontFamily: 'Garamond, serif' }}> 
+      Teknik Informatika (IF-G) </p>
+    <p className="text-black text-3xl" style={{ fontFamily: 'Garamond, serif' }}> 
+      19623215 / 13523021 </p>
+    <p className="text-black text-2xl mt-5" style={{ fontFamily: 'Comic Sans MS, cursive, sans-serif' }}> 
+      Tentang Diri </p>
+    <p className="text-black text-xl text-center ml-45 mr-45 mb-5" style={{ fontFamily: 'Times New Roman, Times, serif' }}> 
+      Seorang mahasiswa semester 3 di Teknik Informatika ITB yang sedang belajar terkait software engineering. 
+      <br/> Memiliki IPK 3.97/4.00 selama masa kuliah dan IQ 133. Ingin menjadi seorang software engineer yang handal.</p>
+    <p className="text-black text-2xl text-center" style={{ fontFamily: 'Comic Sans MS, cursive, sans-serif' }}> 
+      Minat & Bakat </p>
+    <p className="text-black text-xl text-center ml-45 mr-45 mb-5" style={{ fontFamily: 'Times New Roman, Times, serif' }}> 
+      Memiliki minat dalam programming untuk explore di bidang software engineering serta competitive programming.
+      <br/> Memiliki bakat pada logika dan matematika. </p>
+    <p className="text-black text-2xl text-center" style={{ fontFamily: 'Comic Sans MS, cursive, sans-serif' }}> 
+      Proyek-Proyek Sebelumnya </p>
+    <p className="text-black text-xl text-center ml-45 mr-45 mb-5" style={{ fontFamily: 'Times New Roman, Times, serif' }}> 
+      Membuat aplikasi Gojek sederhana yang dapat digunakan sebagaimana Gojek sesungguhnya tetapi hanya pada CLI.
+      <br/> Membuat aplikasi Game Pokemon sederhana yang terdiri atas beragam fitur :
+      <br/> 1. Account
+      <br/> 2. Menu & Help
+      <br/> 3. Inventory
+      <br/> 4. Battle & Arena
+      <br/> 5. Shop & Laboratory
+      <br/> 6. Game Management
+      <br/> 7. Data Load & Save </p>
+    <p className="text-black text-2xl text-center" style={{ fontFamily: 'Comic Sans MS, cursive, sans-serif' }}> 
+      Pencapaian Sebelumnya </p>
+    <p className="text-black text-xl text-center ml-45 mr-45 mb-5" style={{ fontFamily: 'Times New Roman, Times, serif' }}> 
+      Seseorang yang baru terjun di dunia Informatika
+      <br/> Sedang mencari beragam pengalaman & pencapaian di masa kuliah mendatang :) </p>
+    <p className="text-black text-2xl text-center" style={{ fontFamily: 'Comic Sans MS, cursive, sans-serif' }}> 
+      Kontak </p>
+    <p className="text-black text-xl text-left ml-45 mr-45 mb-10" style={{ fontFamily: 'Times New Roman, Times, serif' }}> 
+      Email : m.raihannazhimoktana@gmail.com
+      <br/> LinkedIn : Muhammad Raihan Nazhim Oktana
+      <br/> Line : mraihannazhimoktana
+      <br/> IG : m.raihan_nazhim_o
+      <br/> WA : 081336702067 </p>
+  </main> 
   );
 }
+
+export default Home;
